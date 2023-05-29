bdbb04d99c3fa85afb208e6905a8b56434836d38
Alistair Smith <hi@alistair.sh>
2023-05-29 15:04:17 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# bwitch
+
+A tiny weeny pattern matcher with zero dependencies. Requires TypeScript 5.0+
+
+## Usage
+
+```typescript
+const page = 'home';
+
+const route = bwitch(page)
+	.case('home', () => '/api/home/timeline')
+	.case('weather', () => '/api/v1/weather/forecast')
+	.orThrow();
+
+route; // => `"/api/home/timeline" | "/api/v1/weather/forecast"`
+```
+
+Alternatively, you can finish a bwitch with a default case, or a helper to default to null
+
+```typescript
+const page = 'home';
+
+// Specify default
+const route = bwitch(page)
+	.case('home', () => '/api/home/timeline')
+	// ...
+	.or(value => '/api/home/timeline');
+
+// Default to null
+// This is the same as doing `.or(() => null)`
+const route = bwitch(page)
+	.case('home', () => '/api/home/timeline')
+	// ...
+	.orNull();
+```
---
+++ src/index.ts
@@ -0,0 +1,53 @@
+export type CaseExecutor<In, R> = (value: In) => R;
+export type Case<T, R> = {value: T; execute: CaseExecutor<T, R>};
+
+export interface Expression<T, R> {
+	case: <const In extends T, const Next>(value: In, execute: CaseExecutor<In, Next>) => Expression<T, R | Next>;
+	or: <const Next>(execute: CaseExecutor<T, Next>) => R | Next;
+	orThrow: () => R;
+	orNull: () => R | null;
+}
+
+export function run<T, R>(rootValue: T, cases: Case<T, R>[], or: (value: T) => R): R {
+	for (const c of cases) {
+		if (rootValue === c.value) {
+			return c.execute(rootValue);
+		}
+	}
+
+	return or(rootValue);
+}
+
+export function expression<T, R>(rootValue: T, cases: Case<T, R>[]): Expression<T, R> {
+	return {
+		case: <const In extends T, const Next>(value: In, execute: CaseExecutor<In, Next>) => {
+			const nextCases = [
+				...cases,
+				{
+					value,
+					execute,
+				},
+			] as Case<T, R>[];
+
+			return expression<T, R | Next>(rootValue, nextCases);
+		},
+
+		or: <const Next>(execute: CaseExecutor<T, Next>) => {
+			return run<T, R | Next>(rootValue, cases, value => execute(value));
+		},
+
+		orThrow: () => {
+			return run(rootValue, cases, () => {
+				throw new Error('Unhandled value passed to bwitch');
+			});
+		},
+
+		orNull: () => {
+			return run(rootValue, cases, () => null);
+		},
+	};
+}
+
+export function bwitch<const T, R = never>(value: T) {
+	return expression<T, R>(value, []);
+}
